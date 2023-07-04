ac4449bb63f2230bef8da5594674c78fc4c0bb22
GOKUL-EASWARAN <gokulmedfic@gmail.com>
2023-07-04 23:34:51 +0530

    modified menu still icons and images are pending

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -12,199 +12,227 @@ export default function Index() {
             // content: "4 oz, for those love a meal with their dip",
             item: [
                 {
+                    image: "P23",
+                    name: "Samosa (2)",
+                    content: "Two homemade pastries filled with potatoes, onions and green peas. Fried to perfection served with mint sauce and house sauce",
+                    price: '8',
+                    slug: "samosa"
+                },
+                {
                     image: "P26",
-                    name: "Austin Anarkali",
-                    content: "Dhokla is a supremely healthy and tasty snack made of chickpea batter",
-                    price: '7',
+                    name: "Paratha Melt",
+                    content: "Two parathas stuffed with mozzarella cheese, fresh jalapenos, touch of tikka masala and served with tikka masala sauce. Add chicken extra for $3",
+                    price: '9',
                     slug: "austin-anarkali"
                 },
                 {
                     image: "P24",
-                    name: "Dosa Taco",
-                    content: "Topped with soy keema and cheese",
-                    price: '9',
+                    name: "Chicken Tikka Nacho",
+                    content: "Freshly made to order corn chips topped with cheddar, jack and mozzarella cheese. Chicken tikka masala sauce, pico de Gallo and yogurt sauce",
+                    price: '15',
                     slug: "dosa-taco"
                 },
                 {
                     image: "P23",
-                    name: "Samosa YA! (2)",
-                    content: "Singara is fried pastry with a savory filling of potatoes onions and peas",
-                    price: '7',
+                    name: "Dhokla",
+                    content: "Healthy and nutritious dhokla made of Fermented gram flour and yogurt batter steamed until it is fluffy and delicious, served with mint sauce",
+                    price: '8',
+                    slug: "samosa"
+                },
+                {
+                    image: "P23",
+                    name: "Chicken Saag Naan Pizza",
+                    content: "Naan topped with saag sauce mozzarella cheese, chicken, fresh jalapeno, onion, tomato, pepperoncini and baked to perfection",
+                    price: '12',
                     slug: "samosa"
                 },
             ]
         },
         {
-            name: "DOSAS",
+            name: "DOSA",
             icon: "dosa",
-            content: "Light, crispy crepes-made of rice and lentils – dairy free, gluten - free and delicious - served with two sauces and sambar chowder",
+            content: "Very Light, crispy and healthy crepes made of fermented lentils and rice - fermentation reduces the excessive carbs and increases the micro nutrients - delicious and gluten-free - served with two chutneys and sambar. We only use olive oil for cooking Dosa",
             item: [
                 {
                     image: "p5",
-                    name: "Must-Have Masala",
-                    content: "Potato mix seasoned with mustard seeds",
-                    price: '12',
+                    name: "Masala Dosa",
+                    content: "Traditional masala made of potatoes cooked with onions, peas, seasoned with cumin and mustard seeds. Served with sambar rice",
+                    price: '14',
                     slug: "must-have-masala"
                 },
                 {
                     image: "p6",
-                    name: "Easter Egg",
-                    content: "Crispy dosa topped with 24K edible gold",
-                    price: '12',
+                    name: "Cheese Dosa",
+                    content: "Mozzarella, cheddar jack cheese with cilantro, onions, jalapeno and a touch of tikka masala sauce",
+                    price: '13',
                     slug: "easter-egg"
                 },
                 {
                     image: "p7",
-                    name: "Deepika Padukone",
-                    content: "Topped with fiery hot ghost chilli and stuffed with our potato mix",
-                    price: '12',
+                    name: "Eggs-citement Dosa with Avacado",
+                    content: "3 eggs scrambled with fresh jalapeno onions and cilantro seasoned with salt, pepper, avacado and pico de Gallo. Add cheese extra for $1",
+                    price: '15',
                     slug: "must-have-masala"
                 },
                 {
                     image: "p8",
-                    name: "Cheese in Domain",
-                    content: "Cheese on dosa",
-                    price: '12',
+                    name: "Spinach Artichoke dosa",
+                    content: "Heart of artichokes sauteed with garlic, ginger, fresh baby spinach, grape tomatoes and lemon juice placed on a bed of cream cheese topped with a shaving of fresh parmesan cheese",
+                    price: '15',
                     slug: "cheese-in-domain"
                 },
                 {
                     image: "p10",
-                    name: "Austin to Mars (Kids Special)",
-                    content: "Crepe made of rice and lentils topped with chocolate",
-                    price: '12',
+                    name: "Supreme Dosa with chicken",
+                    content: "Cheddar jeck and mozzarella cheese, onion, cilantro and jalapeno topped with tikka masala sauce and marinated chicken breast. Vegetarian option - Paneer is available",
+                    price: '15',
                     slug: "austin-to-mars"
                 }
             ]
         },
         {
-            name: "ANTI-DOSAS",
+            name: "ENTREES",
             icon: "antidosas",
-            // content: "4 oz, for those love a meal with their dip",
+            content: "All entrees are served with aromatic basmati rice. Add Naan for $2",
             item: [
                 {
                     image: "p5",
-                    name: "Biriyani made for Domain",
-                    content: "Aromatic savory chicken and rice dish",
-                    price: '14',
+                    name: "Chicken Tikka Masala",
+                    content: "The most famous & delicious dish in indian cuisine",
+                    price: '16',
                     slug: "biriyani-made-for-domain"
                 },
                 {
                     image: "p6",
-                    name: "Genghis Khan Tikka Masala",
-                    content: "Tender boneless chicken breast served in a creamy tomato sauce",
-                    price: '14',
+                    name: "Chicken korma",
+                    content: "Marinated chicken breasts cooked in a delicious sauce made of onions, tomatoes, cream, yogurt, cashew paste and spices",
+                    price: '16',
                     slug: "genghis-khan-tikka-masala"
                 },
                 {
                     image: "p7",
-                    name: "Lamb curry",
-                    content: "Lamb cubes cooked with coconut flakes, coconut milk and herbs",
-                    price: '15',
+                    name: "Chicken Saag",
+                    content: "Marinated chicken breasts cooked in a delicious sauce made of spinach, onions, cream and spices",
+                    price: '16',
                     slug: "lamb-curry"
                 },
                 {
                     image: "p1",
-                    name: "Cuddly Idli Sambar",
-                    content: "Steamed savory rice and lentil cakes served with sambar and chutney",
-                    price: '6',
+                    name: "Vegetable Korma",
+                    content: "Delicious korma sauce served with fresh cauliflower, carrots, peas and green beans",
+                    price: '15',
                     slug: "cuddly-idli-sambar"
                 },
                 {
                     image: "p2",
-                    name: "Chia Chia Idly",
-                    content: "A chia superfood variant of the cuddly idli with special tomato sauce",
-                    price: '8',
+                    name: "Paneer Tikka Masala",
+                    content: "Cubes of Paneer(cheese) sauted in butter and served with delicious tikka masala sauce",
+                    price: '15',
+                    slug: "chia-chia-idly"
+                },
+                {
+                    image: "p2",
+                    name: "Palak Paneer",
+                    content: "Cubes of Paneer(cheese) sauted in butter and served with a sauce made of spinach, onions, cream and spices",
+                    price: '15',
+                    slug: "chia-chia-idly"
+                },
+                {
+                    image: "p2",
+                    name: "Fettuccine Chicken Tikka Masala",
+                    content: "Try our delicious chicken tikka masala over fetachini pasta topped with fresh parmesan cheese",
+                    price: '15',
                     slug: "chia-chia-idly"
                 },
             ]
         },
         {
-            name: "DRINKS",
+            name: "BIRIYANI",
             icon: "h30",
-            // content: "Our select favorite alternatives",
+            content: "Onion, tomatoes and fresh jelapeno sauteed with ginger garlic yogurt cinnamon coriander tossed either with chicken or fresh vegetables mixed with rice topped with fried onions and fresh mint. Served with yogurt sauce",
             item: [
                 {
                     image: "p12",
-                    name: "Hari’s Special Chai",
-                    content: "Tea with milk flavored with a mixture of aromatic Indian  and herbs",
-                    price: '3',
+                    name: "Chicken Biriyani",
+                    // content: "Tea with milk flavored with a mixture of aromatic Indian  and herbs",
+                    price: '16',
                     slug: "haris-special-chai"
                 },
                 {
                     image: "p13",
-                    name: "Chennai Coffee",
-                    content: "Chicory-laced coffee poured into furiously hot milk",
-                    price: '3',
+                    name: "Vegetable Biriyani",
+                    // content: "Chicory-laced coffee poured into furiously hot milk",
+                    price: '15',
                     slug: "chennai-coffee"
-                },
-                {
-                    image: "p13",
-                    name: "Regular Ole Drinks",
-                    content: "Coke, diet coke, orange,sprite, dr. pepper,bottled water",
-                    price: '3',
-                    slug: "regular-ole-drinks"
                 },
             ]
         },
         {
-            name: "H30 SHOTS",
+            name: "DESERTS",
             icon: "drinks",
             // content: "Fluids – quench and electrolytic",
             item: [
                 {
                     image: "p12",
-                    name: "Zingy Turmeric",
-                    content: "Turmeric, lemon, ginger",
-                    price: '2',
+                    name: "Gulab Jamun",
+                    content: "Three milk doughs shaped into a golf ball fried and soaked in honey rose syrup and topped with crushed pistachio",
+                    price: '6',
                     slug: 'zingy-turmeric'
                 },
                 {
                     image: "p13",
-                    name: "Fun Fenugreek",
-                    content: "Fenugreek seeds soaked in water",
-                    price: '2',
+                    name: "Ras Malai",
+                    content: "Spongy curved milk flattened and soaked in a creamy sauce flavored with cardamon, saffron and rosewater and topped with crushed pistachio",
+                    price: '7',
+                    slug: 'fun-fenugreek'
+                },
+                {
+                    image: "p13",
+                    name: "Mango Lassi",
+                    content: "Fresh mango pureed with yogurt, cardamom and honey and topped with diced mangoes, whipped cream, fresh strawberry and mint",
+                    price: '6',
                     slug: 'fun-fenugreek'
                 }
             ]
         },
         {
-            name: "Sides/Deserts",
+            name: "DRINKS",
             icon: "drinks",
             // content: "Fluids – quench and electrolytic",
             item: [
                 {
                     image: "p12",
-                    name: "Extra Sambar",
+                    name: "Bottled water",
                     // content: "Potato mix seasoned with mustard seeds",
-                    price: '2',
+                    price: '1.50',
                     slug: 'extra-sambar'
                 },
                 {
                     image: "p13",
-                    name: "Mango Chutney",
+                    name: "Diet Coke",
                     // content: "Potato mix seasoned with mustard seeds",
-                    price: '2',
+                    price: '2.50',
                     slug: 'mango-chutney'
                 },
                 {
                     image: "p13",
-                    name: "Naan",
+                    name: "Mexican Coke",
                     // content: "Potato mix seasoned with mustard seeds",
-                    price: '3',
+                    price: '3.50',
                     slug: 'naan'
                 },
                 {
                     image: "p20",
-                    name: "Danish Pie",
+                    name: "Topo Chico",
                     // content: "Potato mix seasoned with mustard seeds",
-                    price: '5',
+                    price: '3.50',
                     slug: 'danish-pie'
                 },
                 {
                     image: "p21",
-                    name: "Midnight cookie",
+                    name: "Maya's Special Chai",
                     // content: "Potato mix seasoned with mustard seeds",
-                    price: '5',
+                    price: '4.50',
                     slug: 'midnightcookie'
                 },
             ]
@@ -249,7 +277,7 @@ export default function Index() {
                                     </div>
                                     {
                                         popUpShow == i ?
-                                            < div className='popupContainer'>
+                                            <div className='popupContainer'>
                                                 <img src={require("../../assets/popupbg.png")} alt="" className='popUpBg' />
                                                 <div className='menuImageContainer'>
                                                     <img src={require(`../../assets/${e.image}.png`)} alt="" className="menuImage" />
@@ -278,6 +306,6 @@ export default function Index() {
 
             </div>
 
-        </div >
+        </div>
     )
 }
